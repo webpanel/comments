f70b78611196a156b45dc05cf2e57707b26143c4
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2019-09-14 11:58:29 +0200

    Fix export

--- lib/CommentsListResource.d.ts
+++
@@ -1,9 +0,0 @@
-import 'react-chat-widget/lib/styles.css';
-import * as React from 'react';
-export interface ICommentsListProps {
-    referenceID: string | number;
-    inputPosition?: 'top' | 'bottom';
-}
-export declare class CommentsList extends React.Component<ICommentsListProps> {
-    render(): JSX.Element;
-}
